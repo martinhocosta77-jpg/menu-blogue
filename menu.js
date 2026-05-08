78c9f2bf3db57ad8ab7e216fb616a80d40424950
martinhocosta77-jpg <martinhocosta77@gmail.com>
2026-05-08 16:13:12 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,157 +1,69 @@
-<div class="MsoNormal">
-<a href="http://martinho-costa.blogspot.pt/"><span style="color: windowtext;">Martinho Costa</span></a><o:p></o:p></div>
-<div class="MsoNormal">
-<a href="http://martinho-costa.blogspot.pt/p/cv.html"><span style="color: windowtext;">Biography</span></a><o:p></o:p></div>
-<div class="MsoNormal">
-<a href="http://martinhocostatexts.blogspot.pt/"><span style="color: windowtext;">Texts</span></a><o:p></o:p></div>
-<div class="MsoNormal">
-<a href="http://martinho-costa.blogspot.pt/p/contact.html"><span style="color: windowtext;">Contact</span></a><o:p></o:p></div>
-<div class="MsoNormal">
-<a href="http://martinho-costa.blogspot.pt/p/links.html"><span style="color: black;">Links</span></a>
-</div>
-<span style="color: white;">-------------------------------<o:p></o:p></span>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://pinturasarlivre.blogspot.pt/"><span style="color: red;">Pinturas ao Ar Livre</span></a><o:p></o:p></span></div>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://livrodepinturas.blogspot.pt/"><span style="color: red;">Livro de Pinturas</span></a><o:p></o:p></span></div>
+<div style="text-align: left !important; font-family: sans-serif; line-height: 1.5; color: grey;">
 
+  <div style="margin-bottom: 15px;">
+    <a href="http://martinho-costa.blogspot.pt/" style="text-decoration:none; color:black;">Martinho Costa</a><br>
+    <a href="http://martinho-costa.blogspot.pt/p/cv.html" style="text-decoration:none; color:black;">Biography</a><br>
+    <a href="http://martinhocostatexts.blogspot.pt/" style="text-decoration:none; color:black;">Texts</a><br>
+    <a href="http://martinho-costa.blogspot.pt/p/contact.html" style="text-decoration:none; color:black;">Contact</a><br>
+    <a href="http://martinho-costa.blogspot.pt/p/links.html" style="text-decoration:none; color:black;">Links</a>
+  </div>
 
-2025
+  <div style="margin-bottom: 15px;">
+    <a href="http://pinturasarlivre.blogspot.pt/" style="text-decoration:none; color:grey;">Pinturas ao Ar Livre</a><br>
+    <a href="http://livrodepinturas.blogspot.pt/" style="text-decoration:none; color:grey;">Livro de Pinturas</a>
+  </div>
 
-<span style="color: grey;"><a href="https://exposicoes2025.blogspot.com/p/contraforte.html">Contraforte</a></span>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="https://exposicoes2025.blogspot.com/p/forte.html">Forte</a></span>
-</div><o:p></o:p>
+  <div style="color: #eee; margin-bottom: 15px;">-------------------------------</div>
 
-2024
-<div class="MsoNormal">
-<span style="color: grey;"><a href="https://exposicoes2024.blogspot.com/p/casulo-museu-e-centro-de-artes-de.html">Casulo</a></span>
-<div class="MsoNormal">
-<span style="color: lightgrey;"><a href="https://exposicoes2024.blogspot.com/p/cores-vistas-de-dentro-para-fora.html">Cores Vistas de Dentro para Fora</a></span>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="https://exposicoes2024.blogspot.com/p/da-natureza-com-natureza.html"color: lightgrey;">Da natureza, com a Natureza </a></span><o:p></o:p></div><o:p></o:p>
-<div class="MsoNormal">
-<span style="color: lightgrey;"><a href="https://exposicoes2024.blogspot.com/p/a-liberdade-e-so-liberdade-fundacao.html">A liberdade e só a liberdade</a></span>
-<div class="MsoNormal">
-<span style="color: lightgrey;"><a href="https://exposicoes2024.blogspot.com/p/asi-que-pasen-diez-anos.html"color:lightgrey;">Así Que Pasen Diez Años </a></span><o:p></o:p></div><o:p></o:p>
-2023
-<div class="MsoNormal">
-<span style="color: grey;"><a href="https://seriemotordebusca.blogspot.com/"><span style="color: grey;">Motor de Busca</span></a></span><o:p></o:p>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="https://seriefigurainabarcable.blogspot.com/"><span style="color: grey;">Figura Inabarcable</span></a></span><o:p></o:p></div>
-2022
-<div class="MsoNormal">
-<span style="color: grey;"><a href="https://seriedeixeiosolhosnochao.blogspot.com/"><span style="color: grey;">Deixei os Olhos no Chão</span></a></span><o:p></o:p></div>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="https://seriecarocos.blogspot.com/"><span style="color: grey;">Caroços</span></a></span><o:p></o:p></div>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="https://serieoutrostrabalhos22.blogspot.com/">Outros Trabalhos</a></span><o:p></o:p></div>
-2021
-<div class="MsoNormal">
-<span style="color: grey;"><a href="https://seriespacotempomateria.blogspot.com/"><span style="color: grey;">Espaço Tempo Matéria</span></a></span><o:p></o:p></div>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="https://comosinohubier.blogspot.com/"><span style="color: grey;">Como Si No Hubiera Un Mañana</span></a></span><o:p></o:p></div>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="https://seriecasa.blogspot.com"><span style="color: grey;">CASA</span></a></span><o:p></o:p></div>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="https://2021outrostrabalhos.blogspot.com/"><span style="color: grey;">Outros Trabalhos</span></a><o:p></o:p></span></div>
-2020
-<div class="MsoNormal">
-<span style="color: grey;"><a href="https://serieviewfinder.blogspot.com/"><span style="color: grey;">Viewfinder</span></a><o:p></o:p></span></div>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="https://serietestigo.blogspot.com/"><span style="color: grey;">Testigo</span></a><o:p></o:p></span></div>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="https://2020outrostrabalhos.blogspot.com/"><span style="color: grey;">Outros Trabalhos</span></a><o:p></o:p></span></div>
-2019
-<div class="MsoNormal">
-<span style="color: grey;"><a href="https://serie4cortinas.blogspot.com/"><span style="color: grey;">4 Cortinas</span></a><o:p></o:p></span></div>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="https://2019outrostrabalhos.blogspot.com/"><span style="color: grey;">Outros Trabalhos</span></a><o:p></o:p></span></div>
-2018
-<div class="MsoNormal">
-<span style="color: grey;"><a href="https://seriegradienttool.blogspot.pt/"><span style="color: grey;">Gradient Tool</span></a><o:p></o:p></span></div>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://martinho-costa.blogspot.pt/p/outros.html/"><span style="color: grey;">Outros Trabalhos</span></a><o:p></o:p></span></div>
-2017
-<div class="MsoNormal">
-<span style="color: grey;"><a href="https://serieterradesombra.blogspot.pt/"><span style="color: grey;">Terra de Sombra Queimada</span></a><o:p></o:p></span></div>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="https://serieartsantander2017.blogspot.pt/"><span style="color: grey;">Art Santander</span></a><o:p></o:p></span></div>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://martinho-costa.blogspot.pt/p/blog-page.html"><span style="color: grey;">Outros Trabalhos</span></a><o:p></o:p></span></div>
-2016
-<div class="MsoNormal">
-<span style="color: grey;"><a href="https://serielayer0.blogspot.pt/"><span style="color: grey;">Layer 0</span></a><o:p></o:p></span></div>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="https://seriecasaldacegaindub.blogspot.pt/"><span style="color: grey;">Casal da Cega in Dub</span></a><o:p></o:p></span></div>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://seriebaixasfrequencias.blogspot.pt/"><span style="color: grey;">Baixas Frequências</span></a><o:p></o:p></span></div>
-2015
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://serietodososdias.blogspot.pt/"><span style="color: grey;">Todos os Dias Saio Por Um Caminho Diferente</span></a><o:p></o:p></span></div>
+  <div style="margin-bottom: 10px;">
+    <strong style="color:black;">2025</strong><br>
+    <a href="https://exposicoes2025.blogspot.com/p/contraforte.html" style="text-decoration:none; color:grey;">Contraforte</a><br>
+    <a href="https://exposicoes2025.blogspot.com/p/forte.html" style="text-decoration:none; color:grey;">Forte</a>
+  </div>
 
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://projectroompedra.blogspot.pt/"><span style="color: grey;">Pedra</span></a><o:p></o:p></span></div>
+  <div style="margin-bottom: 10px;">
+    <strong style="color:black;">2024</strong><br>
+    <a href="https://exposicoes2024.blogspot.com/p/casulo-museu-e-centro-de-artes-de.html" style="text-decoration:none; color:grey;">Casulo</a><br>
+    <a href="https://exposicoes2024.blogspot.com/p/cores-vistas-de-dentro-para-fora.html" style="text-decoration:none; color:grey;">Cores Vistas de Dentro para Fora</a><br>
+    <a href="https://exposicoes2024.blogspot.com/p/da-natureza-com-natureza.html" style="text-decoration:none; color:grey;">Da natureza, com a Natureza</a><br>
+    <a href="https://exposicoes2024.blogspot.com/p/a-liberdade-e-so-liberdade-fundacao.html" style="text-decoration:none; color:grey;">A liberdade e só a liberdade</a><br>
+    <a href="https://exposicoes2024.blogspot.com/p/asi-que-pasen-diez-anos.html" style="text-decoration:none; color:grey;">Así Que Pasen Diez Años</a>
+  </div>
 
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://2ou3coisassobreela.blogspot.pt/"><span style="color: grey;">Duas Ou Três Coisas Que Eu Sei Sobre Ela</span></a><o:p></o:p></span></div>
-2014
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://serie-unnecessary-repetition.blogspot.pt/"><span style="color: grey;">Unnecessary Repetition of Meaning</span></a><o:p></o:p></span></div>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://serielesstatuesmeurentaussi.blogspot.pt//"><span style="color: grey;">Les Statues Meurent Aussi</span></a><o:p></o:p></span></div>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://outrostrabalhos2014.blogspot.pt/"><span style="color: grey;">Outros Trabalhos</span></a><o:p></o:p></span></div>
-2013
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://serie5artistasemsintra.blogspot.pt/"><span style="color: grey;">5 Artistas em Sintra</span></a><o:p></o:p></span></div>
+  <div style="margin-bottom: 10px;">
+    <strong style="color:black;">2023</strong><br>
+    <a href="https://seriemotordebusca.blogspot.com/" style="text-decoration:none; color:grey;">Motor de Busca</a><br>
+    <a href="https://seriefigurainabarcable.blogspot.com/" style="text-decoration:none; color:grey;">Figura Inabarcable</a>
+  </div>
 
-2012
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://seriethumbnails.blogspot.com/"><span style="color: grey;">Thumbnails</span></a><o:p></o:p></span></div>
+  <div style="margin-bottom: 10px;">
+    <strong style="color:black;">2022</strong><br>
+    <a href="https://seriedeixeiosolhosnochao.blogspot.com/" style="text-decoration:none; color:grey;">Deixei os Olhos no Chão</a><br>
+    <a href="https://seriecarocos.blogspot.com/" style="text-decoration:none; color:grey;">Caroços</a><br>
+    <a href="https://serieoutrostrabalhos22.blogspot.com/" style="text-decoration:none; color:grey;">Outros Trabalhos</a>
+  </div>
 
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://serieaprimeirapedra.blogspot.pt/"><span style="color: grey;">A Primeira Pedra</span></a><o:p></o:p></span></div>
-2011
+  <div style="margin-bottom: 10px;">
+    <strong style="color:black;">2021</strong><br>
+    <a href="https://seriespacotempomateria.blogspot.com/" style="text-decoration:none; color:grey;">Espaço Tempo Matéria</a><br>
+    <a href="https://comosinohubier.blogspot.com/" style="text-decoration:none; color:grey;">Como Si No Hubiera Un Mañana</a><br>
+    <a href="https://seriecasa.blogspot.com" style="text-decoration:none; color:grey;">CASA</a><br>
+    <a href="https://2021outrostrabalhos.blogspot.com/" style="text-decoration:none; color:grey;">Outros Trabalhos</a>
+  </div>
 
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://projectofontainebleu.blogspot.pt/"><span style="color: grey;">Fontainebleau</span></a><o:p></o:p></span></div>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://odiarioderobertstern.blogspot.pt/"><span style="color: grey;">O Diário de Robert Stern</span></a><o:p></o:p></span></div>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://48retratos.blogspot.pt/"><span style="color: grey;">48 Retratos</span></a><o:p></o:p></span></div>
-2010
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://umapinturanapadaria.blogspot.pt/"><span style="color: grey;">Uma Pintura Na Padaria</span></a><o:p></o:p></span></div>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://forcacadegravidade.blogspot.pt/"><span style="color: grey;">Força de Gravidade</span></a><o:p></o:p></span></div>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://superficiefrontal01.blogspot.pt/"><span style="color: grey;">Superfície Frontal 1</span></a><o:p></o:p></span></div>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://serieimagemseguinte.blogspot.pt/"><span style="color: grey;">A Imagem Seguinte</span></a><o:p></o:p></span></div>
-2009
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://superficiefrontal00.blogspot.pt/"><span style="color: grey;">Superfície Frontal</span></a><o:p></o:p></span></div>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://seriereconstrucao.blogspot.pt/"><span style="color: grey;">Reconstrução</span></a><o:p></o:p></span></div>
-2008
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://serieruina.blogspot.pt/"><span style="color: grey;">Ruína</span></a><o:p></o:p></span></div>
-2007
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://seriecolecaoprivada.blogspot.pt/"><span style="color: grey;">Colecção Privada</span></a><o:p></o:p></span></div>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://seriepoeira.blogspot.pt/"><span style="color: grey;">Poeira</span></a><o:p></o:p></span></div>
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://serievolkerwanderung.blogspot.pt/"><span style="color: grey;">Völkerwanderung</span></a><o:p></o:p></span></div>
-2006
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://seriepastoral.blogspot.pt/"><span style="color: grey;">Pastoral</span></a><o:p></o:p></span></div>
-2005
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://seriemaquinacamapanha.blogspot.pt/"><span style="color: grey;">Máquina de Campanha</span></a><o:p></o:p></span></div>
-2004
-<div class="MsoNormal">
-<span style="color: grey;"><a href="http://seriegranturismo.blogspot.pt/"><span style="color: grey;">Gran Turismo</span></a><o:p></o:p></span></div>
-<div class="MsoNormal">
-<span style="color: white;">---------------------------------------</span></div></div></div></div></div></div>
+  <div style="margin-bottom: 10px;">
+    <strong style="color:black;">2020</strong><br>
+    <a href="https://serieviewfinder.blogspot.com/" style="text-decoration:none; color:grey;">Viewfinder</a><br>
+    <a href="https://serietestigo.blogspot.com/" style="text-decoration:none; color:grey;">Testigo</a><br>
+    <a href="https://2020outrostrabalhos.blogspot.com/" style="text-decoration:none; color:grey;">Outros Trabalhos</a>
+  </div>
+
+  <div style="margin-bottom: 10px;">
+    <strong style="color:black;">2019</strong><br>
+    <a href="https://serie4cortinas.blogspot.com/" style="text-decoration:none; color:grey;">4 Cortinas</a><br>
+    <a href="https://2019outrostrabalhos.blogspot.com/" style="text-decoration:none; color:grey;">Outros Trabalhos</a>
+  </div>
+
+  <div style="margin-bottom: 10px;">
+    <strong style="color:black;">2018</strong><br>
+    <a
