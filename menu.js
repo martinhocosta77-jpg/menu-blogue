64089ff640bd653fc4eacc5d4caf3444a34ad302
martinhocosta77-jpg <martinhocosta77@gmail.com>
2026-05-08 16:16:21 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,69 +1,162 @@
-<div style="text-align: left !important; font-family: sans-serif; line-height: 1.5; color: grey;">
+<div style="text-align: left !important; font-family: sans-serif; line-height: 1.5; color: #666;">
 
   <div style="margin-bottom: 15px;">
-    <a href="http://martinho-costa.blogspot.pt/" style="text-decoration:none; color:black;">Martinho Costa</a><br>
-    <a href="http://martinho-costa.blogspot.pt/p/cv.html" style="text-decoration:none; color:black;">Biography</a><br>
-    <a href="http://martinhocostatexts.blogspot.pt/" style="text-decoration:none; color:black;">Texts</a><br>
-    <a href="http://martinho-costa.blogspot.pt/p/contact.html" style="text-decoration:none; color:black;">Contact</a><br>
-    <a href="http://martinho-costa.blogspot.pt/p/links.html" style="text-decoration:none; color:black;">Links</a>
+    <a href="http://martinho-costa.blogspot.pt/" style="text-decoration:none; color:#000;">Martinho Costa</a><br>
+    <a href="http://martinho-costa.blogspot.pt/p/cv.html" style="text-decoration:none; color:#000;">Biography</a><br>
+    <a href="http://martinhocostatexts.blogspot.pt/" style="text-decoration:none; color:#000;">Texts</a><br>
+    <a href="http://martinho-costa.blogspot.pt/p/contact.html" style="text-decoration:none; color:#000;">Contact</a><br>
+    <a href="http://martinho-costa.blogspot.pt/p/links.html" style="text-decoration:none; color:#000;">Links</a>
   </div>
 
   <div style="margin-bottom: 15px;">
-    <a href="http://pinturasarlivre.blogspot.pt/" style="text-decoration:none; color:grey;">Pinturas ao Ar Livre</a><br>
-    <a href="http://livrodepinturas.blogspot.pt/" style="text-decoration:none; color:grey;">Livro de Pinturas</a>
+    <a href="http://pinturasarlivre.blogspot.pt/" style="text-decoration:none; color:#666;">Pinturas ao Ar Livre</a><br>
+    <a href="http://livrodepinturas.blogspot.pt/" style="text-decoration:none; color:#666;">Livro de Pinturas</a>
   </div>
 
   <div style="color: #eee; margin-bottom: 15px;">-------------------------------</div>
 
   <div style="margin-bottom: 10px;">
-    <strong style="color:black;">2025</strong><br>
-    <a href="https://exposicoes2025.blogspot.com/p/contraforte.html" style="text-decoration:none; color:grey;">Contraforte</a><br>
-    <a href="https://exposicoes2025.blogspot.com/p/forte.html" style="text-decoration:none; color:grey;">Forte</a>
+    <strong style="color:#000;">2025</strong><br>
+    <a href="https://exposicoes2025.blogspot.com/p/contraforte.html" style="text-decoration:none; color:#666;">Contraforte</a><br>
+    <a href="https://exposicoes2025.blogspot.com/p/forte.html" style="text-decoration:none; color:#666;">Forte</a>
   </div>
 
   <div style="margin-bottom: 10px;">
-    <strong style="color:black;">2024</strong><br>
-    <a href="https://exposicoes2024.blogspot.com/p/casulo-museu-e-centro-de-artes-de.html" style="text-decoration:none; color:grey;">Casulo</a><br>
-    <a href="https://exposicoes2024.blogspot.com/p/cores-vistas-de-dentro-para-fora.html" style="text-decoration:none; color:grey;">Cores Vistas de Dentro para Fora</a><br>
-    <a href="https://exposicoes2024.blogspot.com/p/da-natureza-com-natureza.html" style="text-decoration:none; color:grey;">Da natureza, com a Natureza</a><br>
-    <a href="https://exposicoes2024.blogspot.com/p/a-liberdade-e-so-liberdade-fundacao.html" style="text-decoration:none; color:grey;">A liberdade e só a liberdade</a><br>
-    <a href="https://exposicoes2024.blogspot.com/p/asi-que-pasen-diez-anos.html" style="text-decoration:none; color:grey;">Así Que Pasen Diez Años</a>
+    <strong style="color:#000;">2024</strong><br>
+    <a href="https://exposicoes2024.blogspot.com/p/casulo-museu-e-centro-de-artes-de.html" style="text-decoration:none; color:#666;">Casulo</a><br>
+    <a href="https://exposicoes2024.blogspot.com/p/cores-vistas-de-dentro-para-fora.html" style="text-decoration:none; color:#666;">Cores Vistas de Dentro para Fora</a><br>
+    <a href="https://exposicoes2024.blogspot.com/p/da-natureza-com-natureza.html" style="text-decoration:none; color:#666;">Da natureza, com a Natureza</a><br>
+    <a href="https://exposicoes2024.blogspot.com/p/a-liberdade-e-so-liberdade-fundacao.html" style="text-decoration:none; color:#666;">A liberdade e só a liberdade</a><br>
+    <a href="https://exposicoes2024.blogspot.com/p/asi-que-pasen-diez-anos.html" style="text-decoration:none; color:#666;">Así Que Pasen Diez Años</a>
   </div>
 
   <div style="margin-bottom: 10px;">
-    <strong style="color:black;">2023</strong><br>
-    <a href="https://seriemotordebusca.blogspot.com/" style="text-decoration:none; color:grey;">Motor de Busca</a><br>
-    <a href="https://seriefigurainabarcable.blogspot.com/" style="text-decoration:none; color:grey;">Figura Inabarcable</a>
+    <strong style="color:#000;">2023</strong><br>
+    <a href="https://seriemotordebusca.blogspot.com/" style="text-decoration:none; color:#666;">Motor de Busca</a><br>
+    <a href="https://seriefigurainabarcable.blogspot.com/" style="text-decoration:none; color:#666;">Figura Inabarcable</a>
   </div>
 
   <div style="margin-bottom: 10px;">
-    <strong style="color:black;">2022</strong><br>
-    <a href="https://seriedeixeiosolhosnochao.blogspot.com/" style="text-decoration:none; color:grey;">Deixei os Olhos no Chão</a><br>
-    <a href="https://seriecarocos.blogspot.com/" style="text-decoration:none; color:grey;">Caroços</a><br>
-    <a href="https://serieoutrostrabalhos22.blogspot.com/" style="text-decoration:none; color:grey;">Outros Trabalhos</a>
+    <strong style="color:#000;">2022</strong><br>
+    <a href="https://seriedeixeiosolhosnochao.blogspot.com/" style="text-decoration:none; color:#666;">Deixei os Olhos no Chão</a><br>
+    <a href="https://seriecarocos.blogspot.com/" style="text-decoration:none; color:#666;">Caroços</a><br>
+    <a href="https://serieoutrostrabalhos22.blogspot.com/" style="text-decoration:none; color:#666;">Outros Trabalhos</a>
   </div>
 
   <div style="margin-bottom: 10px;">
-    <strong style="color:black;">2021</strong><br>
-    <a href="https://seriespacotempomateria.blogspot.com/" style="text-decoration:none; color:grey;">Espaço Tempo Matéria</a><br>
-    <a href="https://comosinohubier.blogspot.com/" style="text-decoration:none; color:grey;">Como Si No Hubiera Un Mañana</a><br>
-    <a href="https://seriecasa.blogspot.com" style="text-decoration:none; color:grey;">CASA</a><br>
-    <a href="https://2021outrostrabalhos.blogspot.com/" style="text-decoration:none; color:grey;">Outros Trabalhos</a>
+    <strong style="color:#000;">2021</strong><br>
+    <a href="https://seriespacotempomateria.blogspot.com/" style="text-decoration:none; color:#666;">Espaço Tempo Matéria</a><br>
+    <a href="https://comosinohubier.blogspot.com/" style="text-decoration:none; color:#666;">Como Si No Hubiera Un Mañana</a><br>
+    <a href="https://seriecasa.blogspot.com" style="text-decoration:none; color:#666;">CASA</a><br>
+    <a href="https://2021outrostrabalhos.blogspot.com/" style="text-decoration:none; color:#666;">Outros Trabalhos</a>
   </div>
 
   <div style="margin-bottom: 10px;">
-    <strong style="color:black;">2020</strong><br>
-    <a href="https://serieviewfinder.blogspot.com/" style="text-decoration:none; color:grey;">Viewfinder</a><br>
-    <a href="https://serietestigo.blogspot.com/" style="text-decoration:none; color:grey;">Testigo</a><br>
-    <a href="https://2020outrostrabalhos.blogspot.com/" style="text-decoration:none; color:grey;">Outros Trabalhos</a>
+    <strong style="color:#000;">2020</strong><br>
+    <a href="https://serieviewfinder.blogspot.com/" style="text-decoration:none; color:#666;">Viewfinder</a><br>
+    <a href="https://serietestigo.blogspot.com/" style="text-decoration:none; color:#666;">Testigo</a><br>
+    <a href="https://2020outrostrabalhos.blogspot.com/" style="text-decoration:none; color:#666;">Outros Trabalhos</a>
   </div>
 
   <div style="margin-bottom: 10px;">
-    <strong style="color:black;">2019</strong><br>
-    <a href="https://serie4cortinas.blogspot.com/" style="text-decoration:none; color:grey;">4 Cortinas</a><br>
-    <a href="https://2019outrostrabalhos.blogspot.com/" style="text-decoration:none; color:grey;">Outros Trabalhos</a>
+    <strong style="color:#000;">2019</strong><br>
+    <a href="https://serie4cortinas.blogspot.com/" style="text-decoration:none; color:#666;">4 Cortinas</a><br>
+    <a href="https://2019outrostrabalhos.blogspot.com/" style="text-decoration:none; color:#666;">Outros Trabalhos</a>
   </div>
 
   <div style="margin-bottom: 10px;">
-    <strong style="color:black;">2018</strong><br>
-    <a
+    <strong style="color:#000;">2018</strong><br>
+    <a href="https://seriegradienttool.blogspot.pt/" style="text-decoration:none; color:#666;">Gradient Tool</a><br>
+    <a href="http://martinho-costa.blogspot.pt/p/outros.html/" style="text-decoration:none; color:#666;">Outros Trabalhos</a>
+  </div>
+
+  <div style="margin-bottom: 10px;">
+    <strong style="color:#000;">2017</strong><br>
+    <a href="https://serieterradesombra.blogspot.pt/" style="text-decoration:none; color:#666;">Terra de Sombra Queimada</a><br>
+    <a href="https://serieartsantander2017.blogspot.pt/" style="text-decoration:none; color:#666;">Art Santander</a><br>
+    <a href="http://martinho-costa.blogspot.pt/p/blog-page.html" style="text-decoration:none; color:#666;">Outros Trabalhos</a>
+  </div>
+
+  <div style="margin-bottom: 10px;">
+    <strong style="color:#000;">2016</strong><br>
+    <a href="https://serielayer0.blogspot.pt/" style="text-decoration:none; color:#666;">Layer 0</a><br>
+    <a href="https://seriecasaldacegaindub.blogspot.pt/" style="text-decoration:none; color:#666;">Casal da Cega in Dub</a><br>
+    <a href="http://seriebaixasfrequencias.blogspot.pt/" style="text-decoration:none; color:#666;">Baixas Frequências</a>
+  </div>
+
+  <div style="margin-bottom: 10px;">
+    <strong style="color:#000;">2015</strong><br>
+    <a href="http://serietodososdias.blogspot.pt/" style="text-decoration:none; color:#666;">Todos os Dias Saio Por Um Caminho Diferente</a><br>
+    <a href="http://projectroompedra.blogspot.pt/" style="text-decoration:none; color:#666;">Pedra</a><br>
+    <a href="http://2ou3coisassobreela.blogspot.pt/" style="text-decoration:none; color:#666;">Duas Ou Três Coisas Que Eu Sei Sobre Ela</a>
+  </div>
+
+  <div style="margin-bottom: 10px;">
+    <strong style="color:#000;">2014</strong><br>
+    <a href="http://serie-unnecessary-repetition.blogspot.pt/" style="text-decoration:none; color:#666;">Unnecessary Repetition of Meaning</a><br>
+    <a href="http://serielesstatuesmeurentaussi.blogspot.pt//" style="text-decoration:none; color:#666;">Les Statues Meurent Aussi</a><br>
+    <a href="http://outrostrabalhos2014.blogspot.pt/" style="text-decoration:none; color:#666;">Outros Trabalhos</a>
+  </div>
+
+  <div style="margin-bottom: 10px;">
+    <strong style="color:#000;">2013</strong><br>
+    <a href="http://serie5artistasemsintra.blogspot.pt/" style="text-decoration:none; color:#666;">5 Artistas em Sintra</a>
+  </div>
+
+  <div style="margin-bottom: 10px;">
+    <strong style="color:#000;">2012</strong><br>
+    <a href="http://seriethumbnails.blogspot.com/" style="text-decoration:none; color:#666;">Thumbnails</a><br>
+    <a href="http://serieaprimeirapedra.blogspot.pt/" style="text-decoration:none; color:#666;">A Primeira Pedra</a>
+  </div>
+
+  <div style="margin-bottom: 10px;">
+    <strong style="color:#000;">2011</strong><br>
+    <a href="http://projectofontainebleu.blogspot.pt/" style="text-decoration:none; color:#666;">Fontainebleau</a><br>
+    <a href="http://odiarioderobertstern.blogspot.pt/" style="text-decoration:none; color:#666;">O Diário de Robert Stern</a><br>
+    <a href="http://48retratos.blogspot.pt/" style="text-decoration:none; color:#666;">48 Retratos</a>
+  </div>
+
+  <div style="margin-bottom: 10px;">
+    <strong style="color:#000;">2010</strong><br>
+    <a href="http://umapinturanapadaria.blogspot.pt/" style="text-decoration:none; color:#666;">Uma Pintura Na Padaria</a><br>
+    <a href="http://forcacadegravidade.blogspot.pt/" style="text-decoration:none; color:#666;">Força de Gravidade</a><br>
+    <a href="http://superficiefrontal01.blogspot.pt/" style="text-decoration:none; color:#666;">Superfície Frontal 1</a><br>
+    <a href="http://serieimagemseguinte.blogspot.pt/" style="text-decoration:none; color:#666;">A Imagem Seguinte</a>
+  </div>
+
+  <div style="margin-bottom: 10px;">
+    <strong style="color:#000;">2009</strong><br>
+    <a href="http://superficiefrontal00.blogspot.pt/" style="text-decoration:none; color:#666;">Superfície Frontal</a><br>
+    <a href="http://seriereconstrucao.blogspot.pt/" style="text-decoration:none; color:#666;">Reconstrução</a>
+  </div>
+
+  <div style="margin-bottom: 10px;">
+    <strong style="color:#000;">2008</strong><br>
+    <a href="http://serieruina.blogspot.pt/" style="text-decoration:none; color:#666;">Ruína</a>
+  </div>
+
+  <div style="margin-bottom: 10px;">
+    <strong style="color:#000;">2007</strong><br>
+    <a href="http://seriecolecaoprivada.blogspot.pt/" style="text-decoration:none; color:#666;">Colecção Privada</a><br>
+    <a href="http://seriepoeira.blogspot.pt/" style="text-decoration:none; color:#666;">Poeira</a><br>
+    <a href="http://serievolkerwanderung.blogspot.pt/" style="text-decoration:none; color:#666;">Völkerwanderung</a>
+  </div>
+
+  <div style="margin-bottom: 10px;">
+    <strong style="color:#000;">2006</strong><br>
+    <a href="http://seriepastoral.blogspot.pt/" style="text-decoration:none; color:#666;">Pastoral</a>
+  </div>
+
+  <div style="margin-bottom: 10px;">
+    <strong style="color:#000;">2005</strong><br>
+    <a href="http://seriemaquinacamapanha.blogspot.pt/" style="text-decoration:none; color:#666;">Máquina de Campanha</a>
+  </div>
+
+  <div style="margin-bottom: 10px;">
+    <strong style="color:#000;">2004</strong><br>
+    <a href="http://seriegranturismo.blogspot.pt/" style="text-decoration:none; color:#666;">Gran Turismo</a>
+  </div>
+
+  <div style="color: #eee; margin-top: 15px;">---------------------------------------</div>
+
+</div>
