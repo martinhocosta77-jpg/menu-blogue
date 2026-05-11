e43ae8dc958305497fc4851b92f6063004b5006c
martinhocosta77-jpg <martinhocosta77@gmail.com>
2026-05-11 08:48:01 +0100

    Update global-layout.js

--- global-layout.js
+++ global-layout.js
@@ -1,1 +1,13 @@
+(function() {
+  document.addEventListener("DOMContentLoaded", function() {
+    // 1. Atualizar o nome/link da home na esquerda de forma centralizada
+    const homeLink = document.querySelector('.nome-home');
+    if (homeLink) {
+      homeLink.innerHTML = "Martinho Costa";
+      homeLink.href = "https://martinho-costa.blogspot.com/";
+    }
 
+    // 2. Controlar o Rodapé ou outros elementos comuns
+    // Podes injetar novos elementos aqui se quiseres
+  });
+})();
